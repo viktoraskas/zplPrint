62ae1c895dfafca6096ac182d00cc70079687ee2
Viktoras Kazbaras <viktoras@vs.lt>
2022-12-18 22:12:12 +0200

    Add project files.

---
+++ zplPrint/models/labelTemplate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zplPrint.models
+{
+    internal class labelTemplate
+    {
+        public List<labelData> Records { get; set; }
+
+    }
+    internal class labelParameters
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Dpi { get; set; }
+        public Boolean Cut { get; set; }
+
+
+    }
+
+    internal class labelData
+    {
+        public string Text { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Margin { get; set; }
+        public int FontSize { get; set; }
+        public int FontWeight { get; set; }
+        public string FontFamily { get; set; }
+        public string FontStyle { get; set; }
+        public int Spacing { get; set; }
+        public Aligment Alignment { get; set; }
+        public Boolean ConvertToGraphic { get; set; }
+    }
+
+    internal enum Aligment { Left, Rigth, Top, Bottom, Center, LeftCenter, RightCenter, TopCenter, BottomCenter,
+                            LeftTop, LeftBottom, RightTop, RightBottom}
+
+}
